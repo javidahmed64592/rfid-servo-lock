054d1cde3d16fd319177e84ec53af55dc81c65b7
Javid Ahmed <javidahmed@icloud.com>
2025-09-27 21:44:22 +0100

    Rename library

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
 """Unit tests for the example.main module."""
 
-from example.main import example_function
+from rfid_servo_lock.main import example_function
 
 
 def test_example_function() -> None:
